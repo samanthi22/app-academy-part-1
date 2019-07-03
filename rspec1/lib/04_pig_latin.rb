82fdb8c1b2aaf134e95ec191331ca7cbd059f788
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2019-07-03 23:14:13 +0000

    Updated code

--- rspec1/lib/04_pig_latin.rb
+++ rspec1/lib/04_pig_latin.rb
@@ -2,25 +2,34 @@ require 'byebug'
 
 def translate(s)
     str = []
+    temp = []
     #byebug
     s = s.split(" ")
     s.each do |word|
-        str << pig_latin_one_word(word)
+        # strip of punctuation
+        word1 = remove_punct(word)
+        str << pig_latin_one_word(word1) + save_punct(word)
+        #str << save_punct(word)
     end
     return str.join(" ")
 end
 
 
 def pig_latin_one_word(s)
-    vowels = "aeiou"
+    vowels = "aeiouyAEIOUY" # added y as a vowel
     s = s.chars
     if vowels.include?(s[0])
-        return s.join + "ay"
+            return s.join + "way"
     else # does not start with vowel
         idx = first_vowel(s.join)
         part1 = s[0...idx]
         part2 = s[idx..-1]
-        return part2.join + part1.join + "ay"
+        first_char = s[0]
+        if first_char == first_char.upcase
+            return part2[0].upcase + part2[1..-1].join + part1.join.downcase + "ay"
+        else 
+            return part2[0..-1].join + part1.join.downcase + "ay"
+        end
     end 
 end
 
@@ -28,7 +37,7 @@ end
 def first_vowel(s)
    first = ""
    s = s.chars
-   vowels = "aeiou"
+   vowels = "aeiouAEIOU"
    s.each.with_index do |ch, i|
        #byebug
       if vowels.include?(ch) 
@@ -42,6 +51,37 @@ def first_vowel(s)
    end
 end
 
+def remove_punct(word)
+    no_punct = []
+    array = "!?.,()&"
+    word = word.split("")
+    word.each do |letter|
+        if array.include?(letter)
+            
+        else
+            no_punct << letter
+        end
+    end
+    return no_punct.join("")
+end 
+
+def save_punct(word)
+   punct = []
+   array = "!?.,()&"
+   word = word.split("") 
+   word.each do |letter|
+       if array.include?(letter)
+           punct << letter
+       end 
+   end
+   punctuation = punct.join("")
+   return punctuation
+end
+
+puts remove_punct("eat...") #=> eat
+puts remove_punct("great!") #=> great
+puts save_punct("eat...") #=> ...
+puts save_punct("great!") #=> !
 # puts first_vowel("banana") #=> 1
 # puts first_vowel("pie") # => 1
 # puts first_vowel("quiet") # => 2 not 1
@@ -53,3 +93,14 @@ end
 # puts pig_latin_one_word("eat")
 # puts pig_latin_one_word("quares") #=> "aresquay"
 # puts pig_latin_one_word("pie")
+puts translate("hello") #=> ellohay
+puts translate("eat") #=> eatway
+puts translate("yellow") #=> yellowway
+puts translate("eat world") #=> eatway orldway
+puts translate("Hello") #=> Ellohay
+puts translate("Apples") #=> Applesway
+puts translate("eat... world?!") #=> eatway... orldway?!
+puts translate("school") #=> ollschay
+puts translate("quick") #=> ickquay
+puts translate("she's great!") #=> e'sshay eatgray!
+ 
